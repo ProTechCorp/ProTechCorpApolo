4572a9658e4179a7f559dd4a9004fa3b00f447fb
Eduardo <eduardo_estela_1999@outlook.com>
2019-10-30 22:28:10 -0500

    Ver 0.7/Entity Family

--- src/main/java/com/protechcorp/platform/model/Family.java
+++ src/main/java/com/protechcorp/platform/model/Family.java
@@ -1,5 +1,31 @@
 package com.protechcorp.platform.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name="families")
 public class Family {
 
+	
+	
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	@NotEmpty(message = "Name cannot be empty")
+	@Column(name="name",nullable=false)
+	private String name;
+	
+	
+	@NotEmpty(message="Description cannot be empty")
+	@Column(name="description",nullable=false)
+	private String description;
+	
 }
