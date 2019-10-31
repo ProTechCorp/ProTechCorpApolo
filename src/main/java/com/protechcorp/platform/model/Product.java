8918c8a0a96d3af8352f9ee70dae378be559bf12
Angelo98 <u201623628@upc.edu.pe>
2019-10-30 21:45:01 -0500

    Ver 3/ Entidad Producto

--- src/main/java/com/protechcorp/platform/model/Product.java
+++ src/main/java/com/protechcorp/platform/model/Product.java
@@ -1,5 +1,79 @@
 package com.protechcorp.platform.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name="products")
 public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotEmpty(message = "You need to specify a Product Name.")
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@NotEmpty(message = "You need to specify a LPN.")
+	@Column(name = "lpn", nullable = false)
+	private String lpn;
+	
+	@NotEmpty(message = "You need to specify a Product brand.")
+	@Column(name = "brand", nullable = false)
+	private String brand;
+	
+	@NotEmpty(message = "You need to specify a description.")
+	@Column(name = "description", nullable = false)
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLpn() {
+		return lpn;
+	}
+
+	public void setLpn(String lpn) {
+		this.lpn = lpn;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
 
 }
