a16e6b6c0b1bb5acdb1a7d72b1111cb81dc0979c
Eduardo <eduardo_estela_1999@outlook.com>
2019-11-08 11:45:42 -0500

    lot

--- src/main/java/com/protechcorp/platform/controller/CategoryController.java
+++ src/main/java/com/protechcorp/platform/controller/CategoryController.java
@@ -1,33 +1,23 @@
 package com.protechcorp.platform.controller;
 
-<<<<<<< HEAD
-import java.util.Optional;
-
-=======
 import java.util.List;
 
 import javax.validation.Valid;
->>>>>>> development
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
->>>>>>> development
 
 import com.protechcorp.platform.model.Category;
 import com.protechcorp.platform.service.ICategoryService;
-
-
 
 @Controller
 @SessionAttributes("category")
@@ -37,21 +27,6 @@ public class CategoryController {
 	@Autowired
 	private ICategoryService categoryService;
 	
-<<<<<<< HEAD
-	@GetMapping("/list")
-	public String listarCategory(Model model) {
-		
-		try {
-			model.addAttribute("listCategory", categoryService.findAll());
-		} catch (Exception e) {
-			model.addAttribute("error",e.getMessage());
-		}
-		return "/category/list";
-	}
-	
-	@GetMapping("/add")
-	public String agregarCaegory(Model model) {
-=======
 	@GetMapping
 	public String listCategories(Model model) throws Exception {
 		
@@ -79,67 +54,29 @@ public class CategoryController {
 			model.addAttribute("title","Save Category");
 			return "category/form";
 		}
->>>>>>> development
 		
 		String mensajeFlash= (category.getId()!= null)? "Edit category" : "Save Category";
 		
-<<<<<<< HEAD
-		return "/category/add";
-=======
 		categoryService.save(category);
 		status.setComplete();
 		flash.addFlashAttribute("success",mensajeFlash);
 		return "redirect:/categories";
->>>>>>> development
 	}
 	
-	@PostMapping("/add")
-	public String agregarCategory(Model model,String name,String description) {
-			
-		
-			
-			
-			Category ev1=new Category();
-			ev1.setName(name);
-			ev1.setDescription(description);
-			
-			
-			try {
-				model.addAttribute("registerCategory", categoryService.save(ev1));
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		
-		return "/category/add";
-	}
 	
-	@GetMapping("/detail/{id}")
-	public String detailCategory(@PathVariable(value="id") Long id,Model model ) {
+	@RequestMapping("/delete")
+	public String deleteCategory(Model model, @RequestParam("id") Long id) {
 		try {
-			Optional<Category> category = categoryService.findById(id);
-			
-			if(!category.isPresent()) {
-				model.addAttribute("info", "Category doesn't exist");
-				return "redirect:/category/list";
-			}
-			else
-				model.addAttribute("category",category.get());
-		} catch (Exception e) {
-			model.addAttribute("error",e.getMessage());
+			categoryService.deleteById(id);
+			model.addAttribute("message", "Successfully Delleted");
 		}
-		
-		return "/category/detail";
+		catch(Exception e){
+			model.addAttribute("message", "Category cannot be deleted.");
+		}
+		model.addAttribute("listCategories", categoryService.getClass());
+		return "/category/listCategories";
+	
 	}
+	
 
 }
-
-
-
-
-
-
-
-
-
--- src/main/java/com/protechcorp/platform/controller/LotController.java
+++ src/main/java/com/protechcorp/platform/controller/LotController.java
@@ -1,5 +1,83 @@
 package com.protechcorp.platform.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.protechcorp.platform.model.Lot;
+import com.protechcorp.platform.service.ILotService;
+
+@Controller
+@SessionAttributes("lot")
+@RequestMapping("/lots")
 public class LotController {
 
+	@Autowired
+	private ILotService lotService;
+	
+	@GetMapping
+	public String listLots(Model model) throws Exception{
+		
+		List<Lot> lots = lotService.findAll();
+		model.addAttribute("lots", lots);
+		
+		return "lot/lot";
+	}
+	
+	@GetMapping(value="/new")
+	public String neLot(Model model) {
+		Lot lot= new Lot();
+		model.addAttribute("lot", lot);
+		model.addAttribute("title", "New Category");
+		return "category/form";
+	}
+	
+		
+	
+	
+	@PostMapping(value="/save")
+	public String saveLot(@Valid Lot lot,
+			BindingResult result, Model model,
+			RedirectAttributes flash,
+			SessionStatus status) throws Exception{
+		
+		
+		if(result.hasErrors()) {
+			model.addAttribute("title", "Save Lot");
+			return "lot/form";
+		}
+		String mensajeFlash=(lot.getId() != null)? "Edit Lot" : "Save Lot";
+		
+		lotService.save(lot);
+		status.setComplete();
+		flash.addFlashAttribute("success", mensajeFlash);
+		return "lot/form";
+	}
+	
+	@RequestMapping("/delete")
+	public String deleteLot(Model model,@RequestParam("id") Long id) {
+		
+	try {
+		lotService.deleteById(id);
+		model.addAttribute("message", "Successfully Delleted");
+	}
+	catch(Exception e) {
+		model.addAttribute("message", "Lot can not be deleted ");
+	}
+	model.addAttribute("listLot", lotService);
+	return "/lot/listLots";
+	}
+	
 }
