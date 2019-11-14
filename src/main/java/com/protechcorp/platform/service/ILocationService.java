5e2e1fbd1788b29ab71adf59bd44975a6fd8aaef
Eduardo <eduardo_estela_1999@outlook.com>
2019-11-14 15:25:34 -0500

    1

---
+++ src/main/java/com/protechcorp/platform/service/ILocationService.java
@@ -0,0 +1,7 @@
+package com.protechcorp.platform.service;
+
+import com.protechcorp.platform.model.Location;
+
+public interface ILocationService extends CrudService<Location> {
+
+}
---
+++ src/test/java/com/protechcorp/platform/ApoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protechcorp.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
