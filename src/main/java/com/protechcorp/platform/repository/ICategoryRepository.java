e8df775d9b710dfbd6485f2d7e86d46852969ca0
Judyth <u201610488@upc.edu.pe>
2019-11-16 20:02:54 -0500

    1

--- src/main/java/com/protechcorp/platform/repository/ICategoryRepository.java
+++ src/main/java/com/protechcorp/platform/repository/ICategoryRepository.java
@@ -1,6 +1,9 @@
 package com.protechcorp.platform.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.protechcorp.platform.model.Category;
@@ -8,5 +11,7 @@ import com.protechcorp.platform.model.Category;
 
 @Repository
 public interface ICategoryRepository extends JpaRepository<Category, Long> {
-
+	
+	@Query("select c from Category c where c.name like %?1%")
+	List<Category> fetchCategoryByName(String name);
 }
