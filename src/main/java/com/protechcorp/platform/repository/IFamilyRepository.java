aee0beafb398a89952bcabf3c0bcae157e9d915a
Judyth <u201610488@upc.edu.pe>
2019-11-16 20:14:04 -0500

    1

--- src/main/java/com/protechcorp/platform/repository/IFamilyRepository.java
+++ src/main/java/com/protechcorp/platform/repository/IFamilyRepository.java
@@ -1,12 +1,17 @@
 package com.protechcorp.platform.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.protechcorp.platform.model.Family;
 
 @Repository
-public interface IFamilyRepository
- extends JpaRepository<Family, Long>{
+public interface IFamilyRepository extends JpaRepository<Family, Long>{
 
+	@Query("select f from Family f where f.name like %?1%")
+	List<Family> fetchFamilyByName(String name);
+	
 }
