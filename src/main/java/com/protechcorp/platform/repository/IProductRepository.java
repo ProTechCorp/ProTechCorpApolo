50cf788973e7a3ab5b2fd67f15a62d4116332fde
NeilBorjas <u201622662@upc.edu.pe>
2019-11-23 16:36:57 -0500

    Vesion final 1.0

--- src/main/java/com/protechcorp/platform/repository/IProductRepository.java
+++ src/main/java/com/protechcorp/platform/repository/IProductRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.protechcorp.platform.model.Family;
 import com.protechcorp.platform.model.Product;
 
 @Repository
